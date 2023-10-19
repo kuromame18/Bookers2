599b2189286c1c5d8857dd39ba63ca53ab1cd906
Hayasaka <146614468+kuromame18@users.noreply.github.com>
2023-10-19 08:44:51 +0000

    削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :books, only: [:index, :create, :show, :edit]
+  resources :books, only: [:index, :create, :show, :edit, :destroy]
   get "/homes/about" => "homes#about", as: "about"
   root to: "homes#top"
 end
