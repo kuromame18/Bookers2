9a5ada3dba0445bb3cdbc93533f5aa856219f7be
Hayasaka <146614468+kuromame18@users.noreply.github.com>
2023-10-20 08:36:07 +0000

    プロフィール編集　追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   resources :books, only: [:index, :create, :show, :edit, :destroy]
+  resources :users, only: [:show, :edit, :update]
   get "/homes/about" => "homes#about", as: "about"
   root to: "homes#top"
 end
